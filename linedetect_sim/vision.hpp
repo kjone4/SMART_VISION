cc678b06779ce1b76aeda0f2b8c6522131de0cff
kim jae won <kjw2001174@gmail.com>
2024-12-04 14:29:38 +0900

    Update vision.hpp

--- linedetect_sim/vision.hpp
+++ linedetect_sim/vision.hpp
@@ -1,1 +1,15 @@
+#ifndef VISION_H
+#define VISION_H
 
+#include "opencv2/opencv.hpp"
+#include <iostream>
+#include <unistd.h>
+#include <sys/time.h>
+#include <signal.h>
+
+using namespace cv;
+using namespace std;
+
+Mat processFrame(Mat frame);
+
+#endif
