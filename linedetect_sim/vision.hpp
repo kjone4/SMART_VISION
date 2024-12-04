f1ba7bc92c39d856558125d36151509e370f423e
kim jae won <kjw2001174@gmail.com>
2024-12-04 15:43:00 +0900

    Update vision.hpp

--- linedetect_sim/vision.hpp
+++ linedetect_sim/vision.hpp
@@ -10,6 +10,8 @@
 using namespace cv;
 using namespace std;
 
-Mat processFrame(Mat frame);
+Mat processFrame(Mat frame); // 화면 이진화
+void get_target(int labeling, Mat stats, Point& center, Mat centroids, double& closest, int& target);
+void draw_target(int labeling, Mat stats, Mat& cutthred, int target, Point center, int& error, Mat centroids);
 
 #endif
